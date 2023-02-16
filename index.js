2cedb3952ebce02a2a0509869d3c33ffa9955696
Heeji Hyun <wlgmlgus@gmail.com>
2023-02-15 20:06:35 -0500

    commit tree 90a20f1b776344906562168843f776b7aa24b79a

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+/* 
+  hasTargetSum([1,2,3,4], 6)
+  seenNumbers = {
+    1: true,
+    2: true,
+    3: true
+  }
+*/
+// function hasTargetSum(array, target){
+//   // create an obj to keep track of numbers we've already seen
+//   const seenNumbers = {}
+//   // iterate through each number in the array
+//   for(let i = 0; i < array.length; i++){
+//     //   for the current num, identify a complement that adds to the target (comp = target - num)
+//     const complement = target - array[i]
+//     //   check if any key on our object is the complement
+//     //     if so, return true
+//     if (seenNumbers[complement]) return true
+//     //     otherwise, add that number to the obj
+//     seenNumbers[array[i]] = true
+//   }
+
+//   // if I reach the end of the array, return false
+//   return false
+// }
+
+
+// Runtime: O(n^2)
+// function hasTargetSum(array, target) {
+//   // Write your algorithm here
+//   for (let i = 0; i < array.length; i++) {
+//     // n steps
+//     const complement = target - array[i]
+//     for (let j = i + 1; j < array.length; j++) {
+//       // n * n steps
+//       if (array[j] === complement) return true
+//     }
+//   }
+//   return false;
+// }
+// to make it simpler 
+// Runtime: O(n)
+function hasTargetSum(array, target){
+  const seenNumbers = {}
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (seenNumbers[complement]) return true;
+    seenNumbers[number] = true;
+  }
+  return false;
+}
+
+
+// O(n) runtime
+function findSock(array) {
+  for(const item of array) {
+    if (item === "sock") return "sock"
+  }
+}
+
+// O(1) runtime
+function findSock(obj) {
+  if(Object.sock) return "sock"
 }
 
 /* 
   Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
+  Runtime: O(n^2)
+  Space: O(n)
 */
 
 /*
